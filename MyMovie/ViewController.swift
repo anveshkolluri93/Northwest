bff37d3cddd8d350e07b9fbd06ccb341d364ff80
Student <s525272@ch110004m.nwmissouri.edu>
2016-03-18 19:14:09 -0500

    Initial Commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Kolluri,Anveshkumar on 3/18/16.
+//  Copyright © 2016 Kolluri,Anveshkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
